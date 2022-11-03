83c56cc997f0e06a55d802b7c037bf11c5f4a8b8
iuliiast <40248982+iuliiast@users.noreply.github.com>
2022-11-03 11:55:53 +0400

    fix duplicated div

--- src/App.jsx
+++ src/App.jsx
@@ -35,16 +35,6 @@ function App() {
 					</div>
 				</section>
 
-				<section className='main_ad'>
-					<div className='wrapper wrapper__main_ad'>
-						<h1 className='main_ad__h1'>АКТИВИРУЙ<br /> ИММУНИТЕТ!</h1>
-						<p className='main_ad__p_bigger'>Всего пять секунд в день помогут укрепить иммунитет<br />и повысить защитные силы организма</p>
-						<img className='main_ad__product_img' src={product} />
-						<h3 className='main_ad__h3'>NOOTRIS ПОМОГАЕТ</h3>
-						<p className='main_ad__p'>Вашему организму во время пандемии<br />и сезонных простуд</p>
-					</div>
-				</section>
-
 				<section className='main_order wrapper wrapper__main_order'>
 					<h2 className='main_order__h2'><span>ПОЛУЧИ ЗАЩИТУ ИММУНИТЕТА</span><br /><span className='main_order__h2_span'>СО СКИДКОЙ -15% ПЕРВЫМ!</span></h2>
 					<p className='main_order__price'><span className='main_order__old_price'>750₽</span>690₽</p>
